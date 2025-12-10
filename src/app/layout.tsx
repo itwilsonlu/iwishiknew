6d33f42d8b43b22d61c8f2ad9491b642041ff546
Wilson Lu <wl54321n@pace.edu>
2025-12-10 13:03:16 -0500

    Add Vercel Analytics integration and update package.json dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 const { SCRIPT_ID, SCRIPT_SRC } = process.env;
@@ -24,7 +25,10 @@ export default function RootLayout({
           <Script defer src={SCRIPT_SRC} data-website-id={SCRIPT_ID} />
         )}
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
